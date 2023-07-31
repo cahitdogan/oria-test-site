f549506046515646cce9158223de1fe34e275558
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-07-31 12:49:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+const waitlistBtn = document.querySelector('.waitlistBtn');
+
+const thankyouPopupBtn = document.querySelector('.thankyou-popupBtn');
+const thankyouPopup = document.querySelector('.thankyou-popup');
+const thankyouOverlay = document.querySelector('.thankyou-overlay');
+const thankyouBtnClosePopup = document.querySelector('.thankyou-close');
+const signUpForTheBeta = document.querySelector(".sign-up-for-the-beta");
+
+waitlistBtn.addEventListener('click',(e)=>{
+    overlay.classList.add('active');
+    popup.classList.add('active');
+})
+
+overlay.addEventListener('click',()=>{
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+})
+
+signUpForTheBeta.addEventListener('click',(e)=>{
+    overlay.classList.add('active');
+    popup.classList.add('active');
+})
+
+
+const thankyouClosePopup = () => {
+    thankyouPopup.classList.remove('active');
+    thankyouOverlay.classList.remove('active');
+}
+
+// waitlistBtn.addEventListener('click', showPopup);
+thankyouBtnClosePopup.addEventListener('click', thankyouClosePopup);
+thankyouOverlay.addEventListener('click', thankyouClosePopup);
