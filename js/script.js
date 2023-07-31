1a6108388b34f158843fbdb4b89bbddd9ebe56ae
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-07-31 15:09:48 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,39 +37,3 @@ thankyouOverlay.addEventListener('click', thankyouClosePopup);
 
 
 
-window.addEventListener("load", function () {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const data = new FormData(form);
-        const action = e.target.action;
-        fetch(action, {
-            method: 'POST',
-            body: data,
-        })
-            .then(() => {
-                redirectToPage();
-            })
-    });
-});
-
-window.addEventListener("load", function () {
-    const form = document.getElementById('my-form-2');
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const data = new FormData(form);
-        const action = e.target.action;
-        fetch(action, {
-            method: 'POST',
-            body: data,
-        })
-            .then(() => {
-                redirectToPage();
-            })
-    });
-});
-
-function redirectToPage() {
-    var url = "thankyou.html";
-    window.location.href = url;
-}
